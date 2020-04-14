25693c4626fdad3e8c5ae7bede71efb4a371d3d0
Rudolf <47896892+nerudxlf@users.noreply.github.com>
2020-04-14 21:18:42 +0600

    The first step
    I did the initial functionality,
    I create four thread. Thread are working fine. They are blocked are using mutex.
    Work messages are displayed on the desktop.

--- main.c
+++ main.c
@@ -1,30 +1,36 @@
-#include <Windows.h>
+#include <windows.h>
 #include <stdio.h>
 #define NUMBER_OF_MAN 4
+#define NUMBER_OF_PIECES_OF_CAKE 20
+#define TIME 5000 //ms
+#define INF INFINITE
 
 HANDLE hMutex;
 
+void thing(int id){
+    printf("The %d is thinking now\n", id);
+    Sleep(TIME*id/30);
+}
+
+void eating(int id){
+    WaitForSingleObject(hMutex, INF);
+    printf("The %d is eatnig now\n", id);
+    ReleaseMutex(hMutex);
+    Sleep(TIME*id/10);
+} 
+
 void dining(void* lpHDining){
-    int id = (int)lpHDining;
-    printf("%d\n", id);
+    DWORD id = (int)lpHDining;;
     while(TRUE){
         thing(id);
         eating(id);
     }
-}
-
-void thing(int id){
-
-}
-
-void eating(int id){
-
+    ExitThread(0);
 }
 
 int main(){
     HANDLE hPeopleDining[NUMBER_OF_MAN];
-    unsigned short int ID[NUMBER_OF_MAN];
-    register int i;
+    register short int i;
 
     hMutex = CreateMutex(NULL, FALSE, NULL);
 
@@ -32,12 +38,19 @@ int main(){
         printf("Error Mutex\n");
         return 0;
     }
-    for(i=0; i<NUMBER_OF_MAN;i++){
-        hPeopleDining[i] = CreateThread(NULL, 0, &dining, (void*)ID[i], 0, NULL);
+    for(i=0;i<NUMBER_OF_MAN;i++){
+        hPeopleDining[i] = CreateThread(NULL, 0, &dining, (void*)(i+1), 0, NULL);
         if(NULL == hPeopleDining[i]){
             printf("Error Thread:%d\n", hPeopleDining[i]);
             return 0;
         }
     }
+
+    WaitForMultipleObjects(NUMBER_OF_MAN, hPeopleDining, TRUE, INF);
+
+    for(i=0; i<NUMBER_OF_MAN;i++){
+        CloseHandle(hPeopleDining[i]);
+    }
+    CloseHandle(hMutex);
     return 0;
 }
